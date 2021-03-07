66cbb6a8f3ddac2604794751f507a5c654fa316f
shima-218 <shima.island.218@gmail.com>
2021-03-07 19:20:19 +0900

    Footer追加

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <template>
-  <router-view/>
+  <div>
+    <router-view />
+    <div class="footer">
+      <Footer />
+    </div>
+  </div>
 </template>
 
 <script>
-export default { 
-
-}
+import Footer from "./components/Footer";
+export default {
+  components: {
+    Footer,
+  },
+};
 </script>
 
 <style>
+div.footer {
+  position: fixed;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>© 2021 shima-218</div>
+</template>
+
+<style scoped>
+div{
+  background-color: #CCFF66;
+}
+</style>
